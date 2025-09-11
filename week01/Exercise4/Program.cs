7e1b5e4ce7078d2ede3ee06a0470043a44504d30
Paul Uagbale <charlesuagbale@gmail.com>
2025-09-11 15:57:46 +0100

    Update Program.cs

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,20 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+class Program {
+    static void Main() {
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter 5 numbers:");
+        for (int i = 0; i < 5; i++) {
+            int input = Convert.ToInt32(Console.ReadLine());
+            numbers.Add(input);
+        }
+
+        int sum = 0;
+        foreach (int n in numbers) {
+            sum += n;
+        }
+
+        Console.WriteLine($"The total sum is {sum}");
     }
 }
