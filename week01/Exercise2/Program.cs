89d4bf00ce562a5159e32a6db44a078122e3852f
Paul Uagbale <charlesuagbale@gmail.com>
2025-09-11 15:50:13 +0100

    Update Program.cs

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,16 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+class Program {
+    static void Main() {
+        Console.Write("Enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number > 0) {
+            Console.WriteLine("That’s a positive number.");
+        } else if (number < 0) {
+            Console.WriteLine("That’s a negative number.");
+        } else {
+            Console.WriteLine("That’s zero.");
+        }
     }
 }
