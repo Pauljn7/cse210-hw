93ca20dad58983589acf9550174e5f143db76f18
Paul Uagbale <charlesuagbale@gmail.com>
2025-09-11 15:59:11 +0100

    Update Program.cs

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,19 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+class Program {
+    // Function to add two numbers together
+    static int Add(int a, int b) {
+        return a + b;
+    }
+
+    static void Main() {
+        Console.Write("Enter first number: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        int result = Add(num1, num2);
+        Console.WriteLine($"The sum of {num1} and {num2} is {result}");
     }
 }
