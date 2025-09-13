0c68f1f26600a2190c7220eb5097a489293c804a
Paul Uagbale <charlesuagbale@gmail.com>
2025-09-13 01:21:42 +0100

    Update Program.cs

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,12 +1,17 @@
 using System;
-class Program {
-    static void Main() {
+class Program
+{
+    static void Main()
+    {
+        // Ask for the user’s name
         Console.Write("What is your name? ");
         string name = Console.ReadLine();
 
-        Console.Write("How old are you? ");
-        int age = Convert.ToInt32(Console.ReadLine());
+        // Ask for their favorite color
+        Console.Write("What is your favorite color? ");
+        string color = Console.ReadLine();
 
-        Console.WriteLine($"Hello {name}, you are {age} years old.");
+        // Output a sentence using variables
+        Console.WriteLine($"{name}, your favorite color is {color}!");
     }
 }
