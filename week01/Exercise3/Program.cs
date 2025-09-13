081d72b182051c9494ba2e89b5cb5f574902fd4e
Paul Uagbale <charlesuagbale@gmail.com>
2025-09-13 01:27:56 +0100

    Update Program.cs

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,37 @@
 using System;
 
-class Program {
-    static void Main() {
-        for (int i = 1; i <= 10; i++) {
-            Console.WriteLine(i);
+class Program
+{
+    static void Main()
+    {
+        // Option 1: Fixed magic number
+        // int magicNumber = 7;
+
+        // Option 2: Random magic number
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        Console.WriteLine("Guess the magic number (between 1 and 100)");
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
         }
     }
 }
