510938b80da14e65ddfda8fe94b92486cca5af43
Paul Uagbale <charlesuagbale@gmail.com>
2025-09-13 01:26:10 +0100

    Update Program.cs

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,16 +1,59 @@
 using System;
 
-class Program {
-    static void Main() {
-        Console.Write("Enter a number: ");
-        int number = Convert.ToInt32(Console.ReadLine());
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter your grade percentage: ");
+        int grade = int.Parse(Console.ReadLine());
 
-        if (number > 0) {
-            Console.WriteLine("That’s a positive number.");
-        } else if (number < 0) {
-            Console.WriteLine("That’s a negative number.");
-        } else {
-            Console.WriteLine("That’s zero.");
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // Add + or - except for F and A+ over 100
+        if (letter != "F")
+        {
+            int lastDigit = grade % 10;
+
+            if (lastDigit >= 7 && grade < 100)
+            {
+                letter += "+";
+            }
+            else if (lastDigit < 3)
+            {
+                letter += "-";
+            }
+        }
+
+        Console.WriteLine($"Your letter grade is: {letter}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
         }
     }
 }
