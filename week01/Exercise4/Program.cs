d4aa7f6848db5202e84e20bd569e4a44955fd211
Paul Uagbale <charlesuagbale@gmail.com>
2025-09-13 01:30:04 +0100

    Update Program.cs

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,20 +1,44 @@
 using System;
+using System.Collections.Generic;
 
-class Program {
-    static void Main() {
+class Program
+{
+    static void Main()
+    {
         List<int> numbers = new List<int>();
+        int input = -1;
 
-        Console.WriteLine("Enter 5 numbers:");
-        for (int i = 0; i < 5; i++) {
-            int input = Convert.ToInt32(Console.ReadLine());
-            numbers.Add(input);
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (input != 0)
+        {
+            Console.Write("Enter number: ");
+            input = int.Parse(Console.ReadLine());
+
+            if (input != 0)
+            {
+                numbers.Add(input);
+            }
         }
 
         int sum = 0;
-        foreach (int n in numbers) {
+        foreach (int n in numbers)
+        {
             sum += n;
         }
 
-        Console.WriteLine($"The total sum is {sum}");
+        double average = (double)sum / numbers.Count;
+        int max = numbers[0];
+        foreach (int n in numbers)
+        {
+            if (n > max)
+            {
+                max = n;
+            }
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {max}");
     }
 }
