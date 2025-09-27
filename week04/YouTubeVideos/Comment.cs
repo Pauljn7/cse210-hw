f0c026a8768adb306707e447c63213dc268c4177
Paul Uagbale <charlesuagbale@gmail.com>
2025-09-27 16:25:32 +0100

    Update Comment.cs

--- week04/YouTubeVideos/Comment.cs
+++ week04/YouTubeVideos/Comment.cs
@@ -1,1 +1,19 @@
+using System;
+
+public class Comment
+{
+    private string _commenterName;
+    private string _text;
+
+    public Comment(string commenterName, string text)
+    {
+        _commenterName = commenterName;
+        _text = text;
+    }
+
+    public void DisplayComment()
+    {
+        Console.WriteLine($"{_commenterName}: {_text}");
+    }
+}
 
