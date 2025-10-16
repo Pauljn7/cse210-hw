fd9341106ccd940938868b723f5770a3dcab9add
Paul Uagbale <charlesuagbale@gmail.com>
2025-10-16 14:52:12 +0100

    Update EternalGoal.cs

--- week06/EternalQuest/EternalGoal.cs
+++ week06/EternalQuest/EternalGoal.cs
@@ -1,1 +1,34 @@
+using System;
+
+namespace EternalQuest
+{
+    public class EternalGoal : Goal
+    {
+        public EternalGoal(string name, string description, int points)
+            : base(name, description, points)
+        {
+        }
+
+        public override int RecordEvent()
+        {
+            // Always gives points, never completes
+            return Points;
+        }
+
+        public override bool IsComplete()
+        {
+            return false;
+        }
+
+        public override void Display(int index)
+        {
+            Console.WriteLine($"{index + 1}. [∞] {Name} ({Description})");
+        }
+
+        public override string GetString()
+        {
+            return $"Eternal|{Name}|{Description}|{Points}";
+        }
+    }
+}
 
