5e6cd0e170a6fef890a1a0998847cb9f1014027c
Paul Uagbale <charlesuagbale@gmail.com>
2025-10-16 18:55:05 +0100

    Implement Activity class with summary and metrics

--- week07/ExerciseTracking/Activity.cs
+++ week07/ExerciseTracking/Activity.cs
@@ -1,1 +1,37 @@
+using System;
+
+namespace ExerciseTracking
+{
+    public abstract class Activity
+    {
+        private DateTime _date;
+        private int _minutes;
+
+        public Activity(DateTime date, int minutes)
+        {
+            _date = date;
+            _minutes = minutes;
+        }
+
+        public int GetMinutes()
+        {
+            return _minutes;
+        }
+
+        public DateTime GetDate()
+        {
+            return _date;
+        }
+
+        public abstract double GetDistance();
+        public abstract double GetSpeed();
+        public abstract double GetPace();
+
+        public virtual string GetSummary()
+        {
+            return $"{_date.ToString("dd MMM yyyy")} {GetType().Name} ({_minutes} min) - " +
+                   $"Distance: {GetDistance():0.0} km, Speed: {GetSpeed():0.0} kph, Pace: {GetPace():0.0} min per km";
+        }
+    }
+}
 
