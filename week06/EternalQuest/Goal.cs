467807a11cfe92321cff92d48e764b310c5b1b38
Paul Uagbale <charlesuagbale@gmail.com>
2025-10-16 14:50:18 +0100

    Update Goal.cs

--- week06/EternalQuest/Goal.cs
+++ week06/EternalQuest/Goal.cs
@@ -1,1 +1,29 @@
+using System;
+
+namespace EternalQuest
+{
+    // Base class for all goal types
+    public abstract class Goal
+    {
+        private string _name;
+        private string _description;
+        private int _points;
+
+        public string Name { get { return _name; } }
+        public string Description { get { return _description; } }
+        public int Points { get { return _points; } }
+
+        public Goal(string name, string description, int points)
+        {
+            _name = name;
+            _description = description;
+            _points = points;
+        }
+
+        public abstract int RecordEvent();
+        public abstract bool IsComplete();
+        public abstract void Display(int index);
+        public abstract string GetString();
+    }
+}
 
