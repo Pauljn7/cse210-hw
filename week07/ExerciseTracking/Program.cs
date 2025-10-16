f132ab92987bac143f9326f3baaed3070a2be4f9
Paul Uagbale <charlesuagbale@gmail.com>
2025-10-16 18:58:08 +0100

    Update Program.cs

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,23 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+namespace ExerciseTracking
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        static void Main(string[] args)
+        {
+            List<Activity> activities = new List<Activity>
+            {
+                new Running(new DateTime(2025, 10, 15), 30, 5.0),
+                new Cycling(new DateTime(2025, 10, 14), 45, 20.0),
+                new Swimming(new DateTime(2025, 10, 13), 25, 40)
+            };
+
+            foreach (Activity activity in activities)
+            {
+                Console.WriteLine(activity.GetSummary());
+            }
+        }
     }
 }
